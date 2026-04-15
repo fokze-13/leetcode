b262d1c484c0173d810f9663c0f71cd9ed97d33b
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-04-15 16:41:16 +0500

    leetcode 75 11

---
+++ leetcode75/task11.py
@@ -0,0 +1,14 @@
+def sol(s: str, t: str) -> bool:
+    if not s:
+        return True
+
+    i = 0
+
+    for j in range(len(t)):
+        if s[i] == t[j]:
+            i += 1
+
+            if i == len(s):
+                return True
+
+    return False

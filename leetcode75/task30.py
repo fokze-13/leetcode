b5139ec49237e00f639cad1169318dbc45eae9aa
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-04-26 16:47:59 +0500

    leetcode task30.py

--- leetcode75/task30.py
+++ leetcode75/task30.py
@@ -1,23 +1,17 @@
-def sign(num: int) -> int:
-    if num < 0:
-        return -1
-    else:
-        return 1
-
 def sol(asteroids: list[int]) -> list[int]:
     stack = []
 
     for a in asteroids:
-        if not stack or sign(a) == sign(stack[-1]):
-            stack.append(a)
+        while stack and a < 0 < stack[-1]:
+            if -a > stack[-1]:
+                stack.pop()
+                continue
+            elif -a == stack[-1]:
+                stack.pop()
+            break
         else:
-            while stack and abs(a) >= abs(stack[-1]):
-                stack.pop() #while bigger
-                print(stack)
-
-            if not stack or abs(a) > abs(stack[-1]):
-                stack.append(a)
+            stack.append(a)
 
     return stack
 
-print(sol([3,5,-6,2,-1,4]))
+print(sol([5, 10, -5]))

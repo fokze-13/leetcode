c5d98e64f4a34c0bf66530675274c604f77c02a1
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-09-19 14:50:47 +0500

    abc cf

--- cf/mex.py
+++ cf/mex.py
@@ -1,18 +1,8 @@
 t = int(input())
 
 for _ in range(t):
-    n = int(input())
-    a = list(map(int, input().split()))
+    inp = map(int, input().split())
 
-    z = o = t = 0
+    mn, md, mx = sorted(inp)
 
-    for num in a:
-        if num % 2 == 0:
-            if (num / 2) % 2 == 0:
-                t += 1
-            else:
-                z += 1
-        else:
-            o += 1
-
-    print(max(z, o , t))
+    print(min(mx-mn, md))

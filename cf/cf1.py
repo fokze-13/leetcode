1fa93aba35ab29f3762ecc14a5b9570a95f57d1a
fokze-13 <108113475+fokze-13@users.noreply.github.com>
2026-09-21 19:45:59 +0500

    cf1

--- cf/cf1.py
+++ cf/cf1.py
@@ -1,8 +1,28 @@
 t = int(input())
 
 for _ in range(t):
-    a, b, c = map(int, input().split())
+    n, k = map(int, input().split())
 
-    diffs = [abs(a - b), abs(a - c), abs(b - c)]
+    if not (n <= k <= 2 * n - 1):
+        print("-1")
+        continue
 
-    print(min(diffs))
+    c = 2 * n - k
+    m = [[0] * n for _ in range(n)]
+
+    for i in range(c):
+        m[i][i] = i + 1
+    for i in range(c, n):
+        m[i][0] = i + 1
+    for j in range(c, n):
+        m[0][j] = n + (j - c) + 1
+
+    cur = k
+    for i in range(n):
+        for j in range(n):
+            if m[i][j] == 0:
+                cur += 1
+                m[i][j] = cur
+
+    for row in m:
+        print(*map(str, row))
